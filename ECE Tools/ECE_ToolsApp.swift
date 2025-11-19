23a9b41eecbf42c1c59c685fbc0658e0d21b687a
Fiona Pao <chiayinpao@gmail.com>
2025-11-19 16:24:37 -0600

    Initial Commit

---
+++ ECE Tools/ECE_ToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECE_ToolsApp.swift
+//  ECE Tools
+//
+//  Created by Fiona Pao on 2025/11/19.
+//
+
+import SwiftUI
+
+@main
+struct ECE_ToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
